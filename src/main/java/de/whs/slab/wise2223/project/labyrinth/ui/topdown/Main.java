2a04766a4917ba30b4a03b524e86dd569460b66d
Nik Benson <nbenson@nettrek.de>
2022-11-22 13:51:50 +0100

    feat(topdown): level from file

--- src/main/java/de/whs/slab/wise2223/project/labyrinth/ui/topdown/Main.java
+++ src/main/java/de/whs/slab/wise2223/project/labyrinth/ui/topdown/Main.java
@@ -1,5 +1,6 @@
 package de.whs.slab.wise2223.project.labyrinth.ui.topdown;
 
+import de.whs.slab.wise2223.project.labyrinth.level.LevelProvider;
 import de.whs.slab.wise2223.project.labyrinth.model.Level;
 import de.whs.slab.wise2223.project.labyrinth.ui.StreamLevelProvider;
 import de.whs.slab.wise2223.project.labyrinth.ui.model.Maze2D;
@@ -8,14 +9,29 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import processing.core.PApplet;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Main extends PApplet {
+    private final LevelProvider levelProvider;
     private Maze2D maze;
 
+    public Main(LevelProvider levelProvider) {
+        this.levelProvider = levelProvider;
+    }
+
     public static void main(String[] args) {
-        Main newMain = new Main();
+        LevelProvider level = null;
+        try {
+            if (args.length != 1) throw new FileNotFoundException();
+
+            level = new StreamLevelProvider(new FileInputStream(args[0]));
+        } catch (FileNotFoundException e) {
+            level = new StreamLevelProvider(System.in);
+        }
+        Main newMain = new Main(level);
 
         //Give the maze the reference to this, so it can draw
         newMain.maze = new Maze2D(newMain);
@@ -26,8 +42,7 @@ public class Main extends PApplet {
     public void settings() {
         size(600, 480);
 
-        Level level = new StreamLevelProvider(System.in).getLevel();
-        maze.fillMaze(level);
+        maze.fillMaze(levelProvider.getLevel());
     }
 
     public void draw() {
